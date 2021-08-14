f56ace5a0614c63ee7dfb15bb4aa13c5c85b6c96
kush02122001 <54857265+kush02122001@users.noreply.github.com>
2021-08-14 21:52:36 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,12 @@
+
+
+
+r = int(input("input the radius of the circle:"))
+a = r * r * 3.14
+print ("Area of the circle with radius", r, " is", a )
+
+file_name = input("Enter Filename: ")
+
+K = file_name.split(".")
+
+print ("Extension of the file is : " + K[-1])
