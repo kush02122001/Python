8d6c4152d79178c41638f6de30271f2d32551db7
kush02122001 <54857265+kush02122001@users.noreply.github.com>
2021-08-30 18:30:16 +0530

    Update mississipi.py

--- mississipi.py
+++ mississipi.py
@@ -1,10 +1,11 @@
-WD = input()
+def most_frequent():
+    wd = 0
+wd = input("please enter a string")
 m = 0
 a = 0
 s = 0
 p = 0
-
-for i in WD : 
+for i in wd:
     if i == "m":
         m+=1
     if i == "i": 
@@ -13,8 +14,9 @@ for i in WD :
         s+=1
     if i == "p":
         p+=1
-
 print ("i = 0", a)
 print ("s = 0", s)
 print ("p = 0", p)
 print ("m = 0", m)
+
+most_frequent()
