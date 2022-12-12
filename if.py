60af823569d91f9938e44801a609264a933ffb37
kush lad <54857265+kush02122001@users.noreply.github.com>
2022-12-12 23:44:20 +0530

    Add files via upload

---
+++ if.py
@@ -0,0 +1,13 @@
+x = int(input("write a random number"))
+
+if(x<10):
+  print("you have written a number less than 10" )
+if(x>20):
+    print("you have written a numebr more than 20")
+   
+x=0
+if x<2:
+    print("small")
+elif x<10:
+  print("big")
+          
